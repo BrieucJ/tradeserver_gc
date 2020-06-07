e0e0daf74accdd9b680f7c813d8391ac695e7ef3
Brieuc <brieucjamain@gmail.com>
2020-06-07 22:12:26 +0200

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '3j98+c=+v5^pu&6na&m%t@&&nf7rsu1uhp_lu_75$&lb#30h1y'
 
 #CELERY
 CELERY_BROKER_URL = 'amqp://localhost'
