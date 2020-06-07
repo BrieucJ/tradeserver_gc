01d74ea878d600699bfd96cdfead2f005cdecaa9
Brieuc <brieucjamain@gmail.com>
2020-06-07 22:03:48 +0200

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 #CELERY
 CELERY_BROKER_URL = 'amqp://localhost'
@@ -142,7 +142,6 @@ STATIC_URL = '/static/'
 
 # Place static in the same location as webpack build files
 STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
-print(STATIC_ROOT)
 STATICFILES_DIRS = []
 
 # If you want to serve user uploaded files add these settings
