3c09d19a45eac940d6f3417c548fb1100f9c43ab
Brieuc <brieucjamain@gmail.com>
2020-06-07 18:06:09 +0200

    test

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from core import views
+from api import views
 from .views import index
 
 router = routers.DefaultRouter()
