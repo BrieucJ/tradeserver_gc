92e6e7d52ee039dc9c3bbd0db5d85d3ce5c609eb
Brieuc <brieucjamain@gmail.com>
2020-06-19 18:10:45 +0200

    test

--- api/trade/etoro.py
+++ api/trade/etoro.py
@@ -27,7 +27,7 @@ class API():
         self.options.add_argument(f'user-agent={self.user_agent}')
         # self.options.add_argument("--window-size=1920,1080")
         # self.options.add_argument("--incognito")
-        #self.browser = webdriver.Chrome(executable_path=str(os.environ.get('CHROMEDRIVER_PATH')), options=self.options)
+        self.browser = webdriver.Chrome(executable_path=str(os.environ.get('CHROMEDRIVER_PATH')), options=self.options)
         #self.browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {"source": """ Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"""}) #inject js script to hide selenium webdriveer
         self.wait = WebDriverWait(self.browser, 200)
         self.browser.implicitly_wait(200)
