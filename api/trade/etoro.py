c0b67a037278f3ca2693d4d7ab39bd8c1f492304
Brieuc <brieucjamain@gmail.com>
2020-06-20 19:12:47 +0200

    test

--- api/trade/etoro.py
+++ api/trade/etoro.py
@@ -60,7 +60,7 @@ class API():
             pass
         else:
             print('switching')
-            self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "et-select")))
+            self.wait.until(EC.element_to_be_clickable((By.TAG_NAME, "et-select")))
             switch_mode_btn = self.browser.find_element_by_tag_name('et-select')
             switch_mode_btn.click()
             switch_real_btn = self.browser.find_element_by_tag_name('et-select-body').find_elements_by_tag_name('et-select-body-option')[0]
