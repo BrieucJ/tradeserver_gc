2d5ccdc9ec6a382c86a5b8a701a2da41241b149c
Brieuc <brieucjamain@gmail.com>
2020-06-21 20:47:24 +0200

    test

--- api/trade/etoro.py
+++ api/trade/etoro.py
@@ -50,11 +50,12 @@ class API():
         element = self.browser.find_element_by_tag_name('header').find_element_by_xpath('..')
         if ('demo-mode' not in element.get_attribute('class').split() and self.mode == 'real') or ('demo-mode' in element.get_attribute('class').split() and self.mode == 'demo'):
             print('Current mode == selected mode')
-            pass
         else:
-            print('switching')
+            print('Switching')
             # self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "et-select[automation-id='menu-layout-link-mode']")))
-            self.browser.find_element_by_css_selector("et-select[automation-id='menu-layout-link-mode']").click()
+            switch_btn = self.browser.find_element_by_css_selector("et-select[automation-id='menu-layout-link-mode']")
+            print(switch_btn)
+            switch_btn.click()
             print('click')
             if self.mode == 'real':
                 print('Switching from demo to real')
