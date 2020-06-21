0897e0632ef82c57a67736633e52052c2ffca898
Brieuc <brieucjamain@gmail.com>
2020-06-21 22:02:31 +0200

    test

--- api/trade/etoro.py
+++ api/trade/etoro.py
@@ -38,7 +38,10 @@ class API():
         password_field.send_keys(self.password)
         submit_btn.click()
         self.wait.until(lambda driver: self.browser.current_url == 'https://www.etoro.com/watchlists')
-        self.switch_mode()
+        if self.browser.current_url == '':
+            self.logged_in = True
+            self.switch_mode()
+        assert(self.logged_in)
 
     def switch_mode(self):
         element = self.browser.find_element_by_tag_name('header').find_element_by_xpath('..')
@@ -46,6 +49,7 @@ class API():
             print('Current mode == selected mode')
         else:
             print('Switching')
+            self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "et-select[automation-id='menu-layout-link-mode']")))
             switch_btn = self.browser.find_element_by_css_selector("et-select[automation-id='menu-layout-link-mode']")
             print(switch_btn.text)
             switch_btn.click()
