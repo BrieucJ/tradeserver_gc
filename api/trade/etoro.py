c27ce7500885f0eb54422fc9abb7e5d2bb01b4e9
Brieuc <brieucjamain@gmail.com>
2020-06-22 18:24:23 +0200

    test

--- api/trade/etoro.py
+++ api/trade/etoro.py
@@ -18,9 +18,9 @@ class API():
         self.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
         self.options = webdriver.ChromeOptions()
         self.options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+        self.options.add_argument('--no-sandbox')
         self.options.add_argument('--headless')
         self.options.add_argument('--disable-dev-shm-usage')
-        self.options.add_argument('--no-sandbox')
         # self.options.add_argument("--window-size=1920,1080")
         self.options.add_argument(f'user-agent={self.user_agent}')
         self.browser = webdriver.Chrome(executable_path=str(os.environ.get('CHROMEDRIVER_PATH')), chrome_options=self.options)
@@ -51,7 +51,7 @@ class API():
         else:
             print('Switching')
             switch_btn = self.browser.find_element_by_tag_name('et-select')
-            menu = switch_btn.find_elements_by_tag_name('et-select-body')
+            menu = switch_btn.find_element_by_tag_name('et-select-body')
             print(switch_btn.text)
             print(menu.text)
             # self.browser.execute_script("arguments[0].click();", switch_btn)
