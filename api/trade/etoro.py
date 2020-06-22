6f8704f9369b06995ebbf34be58142aafeb40d7c
Brieuc <brieucjamain@gmail.com>
2020-06-23 00:49:51 +0200

    test

--- api/trade/etoro.py
+++ api/trade/etoro.py
@@ -44,7 +44,7 @@ class API():
             print('LOGGED IN')
             self.logged_in = True
             user_name = self.browser.find_element_by_css_selector("a[automation-id='menu-user-page-link']")
-            print(user_name.)
+            print(user_name)
             test = user_name.get_attribute['href']
             print(test)
         else:
