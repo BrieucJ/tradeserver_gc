4996959e82a8e390258f6aaebb14e9ea5db78b50
Brieuc <brieucjamain@gmail.com>
2020-06-30 20:34:09 +0200

    test

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,2 +1,9 @@
-export const API_URL =  'http://localhost:8000/' //"http://traderserver.herokuapp.com/" //'http://localhost:8000/'  // 
+
+if (process.env.NODE_ENV == 'development') {
+    var API_URL =  'http://localhost:8000/'
+} else {
+    var API_URL =  'https://localhost:8000/'
+}
+
+export var API_URL
 export const APP_NAME = 'TraderServer'
