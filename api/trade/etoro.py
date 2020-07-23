db3400e8679d110897f062fbf9ab3029bed63e67
Brieuc <brieucjamain@gmail.com>
2020-07-24 01:39:34 +0200

    test

--- api/trade/etoro.py
+++ api/trade/etoro.py
@@ -14,6 +14,7 @@ class API():
         print('API __init__')
         self.mode = mode
         print(self.mode)
+        print(settings.PRODUCTION)
         self.user_name = broker_username
         self.password = broker_password
         self.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
