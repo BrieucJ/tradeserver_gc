889e6afc5cf0decd116fbb4428213f758f51cd7f
Brieuc <brieucjamain@gmail.com>
2020-07-24 04:58:34 +0200

    test

--- api/trade/etoro.py
+++ api/trade/etoro.py
@@ -21,7 +21,7 @@ class API():
         self.password = broker_password
         self.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
         self.options = webdriver.ChromeOptions()
-        self.options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+        self.options.binary_location = os.environ['GOOGLE_CHROME_BIN']
         self.options.add_argument('--no-sandbox')
         self.options.add_argument('--disable-dev-shm-usage')
         # self.options.add_argument("--disable-gpu")
@@ -31,7 +31,7 @@ class API():
         self.options.add_argument('--headless')
         if settings.PRODUCTION:
             print('USING PROD SETTINGS')
-            self.browser = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), options=self.options)
+            self.browser = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER_PATH'], options=self.options)
             print(self.browser.capabilities['version'])
         else:
             print('USING DEV SETTINGS')
