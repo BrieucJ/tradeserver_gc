0e088cb6813ffbf915745ee33367339992b689ba
Brieuc <brieucjamain@gmail.com>
2020-08-04 17:03:26 +0200

    test

--- backend/settings.py
+++ backend/settings.py
@@ -171,8 +171,8 @@ locals()['DATABASES']['default'] = dj_database_url.config(conn_max_age=django_he
 
 #CELERY
 CELERY_TASK_SERIALIZER='json'
-CELERY_MAX_TASKS_PER_CHILD=1
-CELERY_WORKER_MAX_MEMORY_PER_CHILD = 500000
+worker_max_tasks_per_child = 1
+# CELERYD_WORKER_MAX_MEMORY_PER_CHILD = 500000
 CELERY_TIMEZONE = 'Europe/Paris'
 # BROKER_POOL_LIMIT = 3
 if PRODUCTION:
